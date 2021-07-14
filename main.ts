8f462b8dcc197497e944a76da7b9f051eb1f3763
Donutian <86685997+Donutian@users.noreply.github.com>
2021-07-14 09:28:03 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,56 @@
-
+controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
+    jetExhaust()
+})
+controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+    jetExhaust()
+})
+function jetExhaust () {
+    flame = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 2 4 5 . . . . . . . . 
+        . . . . 2 4 5 5 . . . . . . . . 
+        . . . . . 2 4 5 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Thunder_Wing, -1, 0)
+    flame.destroy(effects.clouds, 100)
+}
+controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
+    jetExhaust()
+})
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+    jetExhaust()
+})
+let flame: Sprite = null
+let Thunder_Wing: Sprite = null
+Thunder_Wing = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    .ff.....................
+    .fbb....................
+    ..bbb...................
+    ..bbbbb.....bb1555......
+    .fbbbbbbbbbbbb1111bb....
+    .fbbbbbbbbbbbbbbbbbbb...
+    ..bbbbbffffffffffbbbbff.
+    ...bb..fbbbbbbbbb.......
+    ......bbbbbbbbbbd.......
+    .....bbbbbbbbbdd........
+    ....bbbbbbbddd..........
+    ........................
+    `, SpriteKind.Player)
+controller.moveSprite(Thunder_Wing, 100, 100)
+scene.cameraFollowSprite(Thunder_Wing)
+tiles.setTilemap(tilemap`level1`)
---
+++ tilemap.g.ts
@@ -0,0 +1,103 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`200010000101010202020201010101010101010101010101010101010101010101010101010102020202020202010101010101010101010101010101010101010101010101010202020202020202020101010101010101010101010101010102020101010101010202020202020202010101010101020101010101010101020202020101010101010202020202020101010101010102020101010101010102020202010101010101020202010101010101010101010101010101010101010102020201010101010101020101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010201010101010101010101010101010101010101010101010101010101010102020201010101010101010101010101010101010101010101010101010102020202020201010101010101010101010101010202020101010101010101010102020202010101010101010101010101010102020202010101010101010101010101010101010101010101010101010101010202020201010101010101010101010101010101010101010101010101010101010202010101010101010101010101010101010101010101010202010101010101010101010101010101010101010101010101010101010101020101010101010101010101010101010101010101010101010101010101010101010101`, img`
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+`, [myTiles.transparency16,myTiles.tile9,myTiles.tile1,myTiles.tile18], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile14":
+            case "tile16":return tile16;
+            case "myTile16":
+            case "tile18":return tile18;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
