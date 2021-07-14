fed8c058f1aa1e68520edb8a9c8a9d6402adc44b
Donutian <86685997+Donutian@users.noreply.github.com>
2021-07-14 11:01:06 -0700

    Some music was added. Music not complete

--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
 controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
     jetExhaust()
 })
+function A_to_E () {
+    music.playTone(440, music.beat(BeatFraction.Sixteenth))
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
+    music.playTone(988, music.beat(BeatFraction.Eighth))
+    music.playTone(740, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (thunderWingVelocity < 0) {
         missile = sprites.createProjectileFromSprite(img`
@@ -61,11 +75,42 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
             `, Thunder_Wing, 150, 0)
     }
 })
+function G_to_E () {
+    music.playTone(392, music.beat(BeatFraction.Sixteenth))
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
+    music.playTone(988, music.beat(BeatFraction.Eighth))
+    music.playTone(740, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+}
 controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
     jetExhaust()
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    Thunder_Wing.image.flipX()
+    Thunder_Wing.setImage(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        .....................ff.
+        ....................bbf.
+        ...................bbb..
+        ......5555bb.....bbbbb..
+        ....bb5555bbbbbbbbbbbbf.
+        ...bbbbbbbbbbbbbbbbbbbf.
+        .ffbbbbffffffffffbbbbb..
+        .......bbbbbbbbbf..bb...
+        .......dbbbbbbbbbb......
+        ........ddbbbbbbbbb.....
+        ..........dddbbbbbbb....
+        ........................
+        `)
 })
 function jetExhaust () {
     flame = sprites.createProjectileFromSprite(img`
@@ -86,11 +131,86 @@ function jetExhaust () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `, Thunder_Wing, -1, 0)
-    flame.destroy(effects.clouds, 100)
+    flame.destroy(effects.clouds, 50)
+}
+function Ab_to_E () {
+    music.playTone(415, music.beat(BeatFraction.Sixteenth))
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
+    music.playTone(988, music.beat(BeatFraction.Eighth))
+    music.playTone(740, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+}
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    Thunder_Wing.setImage(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        .ff.....................
+        .fbb....................
+        ..bbb...................
+        ..bbbbb.....bb5555......
+        .fbbbbbbbbbbbb5555bb....
+        .fbbbbbbbbbbbbbbbbbbb...
+        ..bbbbbffffffffffbbbbff.
+        ...bb..fbbbbbbbbb.......
+        ......bbbbbbbbbbd.......
+        .....bbbbbbbbbdd........
+        ....bbbbbbbddd..........
+        ........................
+        `)
+})
+function gameMusic () {
+    music.setTempo(30)
+    for (let index = 0; index < 3; index++) {
+        B_to_E()
+        A_to_E()
+        Ab_to_E()
+        G_to_E()
+    }
+    B_to_E()
+    A_to_E()
+    Ab_to_E()
+    G_to_C()
 }
 controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
     jetExhaust()
 })
+function G_to_C () {
+    music.playTone(392, music.beat(BeatFraction.Sixteenth))
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
+    music.playTone(988, music.beat(BeatFraction.Eighth))
+    music.playTone(740, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.playTone(554, music.beat(BeatFraction.Eighth))
+}
+function B_to_E () {
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
+    music.playTone(988, music.beat(BeatFraction.Eighth))
+    music.playTone(740, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(698, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+}
 controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
     jetExhaust()
 })
@@ -120,3 +240,4 @@ controller.moveSprite(Thunder_Wing, 100, 100)
 scene.cameraFollowSprite(Thunder_Wing)
 tiles.setTilemap(tilemap`level1`)
 thunderWingVelocity = Thunder_Wing.vx
+gameMusic()
