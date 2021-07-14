75d66b03a313a52485fbcc74661f222335a339b0
Donutian <86685997+Donutian@users.noreply.github.com>
2021-07-14 09:59:59 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,71 @@
 controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
     jetExhaust()
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (thunderWingVelocity < 0) {
+        missile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . d d d d d d d . . . . . . . 
+            . . d d d d d d d d . . . . . . 
+            . . d d d d d d d . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, Thunder_Wing, -150, 0)
+    } else if (thunderWingVelocity == 100) {
+        missile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . d d d d d d d . . . . . . . 
+            . . d d d d d d d d . . . . . . 
+            . . d d d d d d d . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, Thunder_Wing, 150, 0)
+    } else if (thunderWingVelocity == 0) {
+        missile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . d d d d d d d . . . . . . . 
+            . . d d d d d d d d . . . . . . 
+            . . d d d d d d d . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, Thunder_Wing, 150, 0)
+    }
+})
 controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
     jetExhaust()
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    Thunder_Wing.image.flipX()
 })
 function jetExhaust () {
     flame = sprites.createProjectileFromSprite(img`
@@ -32,6 +95,8 @@ controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
     jetExhaust()
 })
 let flame: Sprite = null
+let missile: Sprite = null
+let thunderWingVelocity = 0
 let Thunder_Wing: Sprite = null
 Thunder_Wing = sprites.create(img`
     ........................
@@ -41,8 +106,8 @@ Thunder_Wing = sprites.create(img`
     .ff.....................
     .fbb....................
     ..bbb...................
-    ..bbbbb.....bb1555......
-    .fbbbbbbbbbbbb1111bb....
+    ..bbbbb.....bb5555......
+    .fbbbbbbbbbbbb5555bb....
     .fbbbbbbbbbbbbbbbbbbb...
     ..bbbbbffffffffffbbbbff.
     ...bb..fbbbbbbbbb.......
@@ -54,3 +119,4 @@ Thunder_Wing = sprites.create(img`
 controller.moveSprite(Thunder_Wing, 100, 100)
 scene.cameraFollowSprite(Thunder_Wing)
 tiles.setTilemap(tilemap`level1`)
+thunderWingVelocity = Thunder_Wing.vx
